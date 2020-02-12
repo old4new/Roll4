9cafc8a141b208eafd6376bbe4f92dff62d322ea
old4new <old4new@users.noreply.github.com>
2020-02-12 00:08:10 +0000

    Improved structure, fewer hacks, less reliance on ID

--- script.js
+++ script.js
@@ -7,26 +7,16 @@ const sortButton = document.getElementById('sort');
 
 const testButton = document.getElementById('testButton');
 
-let rollBox1 = document.getElementById('roll1');
-let rollBox2 = document.getElementById('roll2');
-let rollBox3 = document.getElementById('roll3');
 
-//let combatant1Div = document.getElementById('combatant1');
-//let combatant2Div = document.getElementById('combatant2');
-//let combatant3Div = document.getElementById('combatant3');
-let combatantDivs = document.getElementsByClassName('combatant');
 
-//let nameBox1 = document.getElementById('combatant1_name');
-let nameBox2 = document.getElementById('combatant2_name');
-let nameBox3 = document.getElementById('combatant3_name');
 
 let initiativeBox = document.getElementById('initiative-box');
+let combatantDivs = document.getElementsByClassName('combatant');
+let nameBoxArr = document.querySelectorAll('h4.name');
+let rollBoxArr = document.querySelectorAll('.roll');
 
 
-
-
-
- let combatants = [
+let combatants = [
 	{
 		nature: "monster",
 		name: "Orc1",
@@ -35,8 +25,9 @@ let initiativeBox = document.getElementById('initiative-box');
 		initModifier: 2,
 		armour: 14,
 		initiative: 10,
-		nameBox: document.getElementById('combatant1_name'),
-		rollBox: rollBox1
+		comBox: combatantDivs[0],
+		nameBox: nameBoxArr[0],
+		rollBox: rollBoxArr[0]
 	},
 	{
 		name: "Ziggy",
@@ -45,8 +36,9 @@ let initiativeBox = document.getElementById('initiative-box');
 		initModifier: 2,
 		armour: 16,
 		initiative: 10,
-		nameBox: document.getElementById('combatant2_name'),
-		rollBox: rollBox2
+		comBox: combatantDivs[1],
+		nameBox: nameBoxArr[1],
+		rollBox: rollBoxArr[1]
 	},
 	{
 		name: "Orvex",
@@ -55,20 +47,19 @@ let initiativeBox = document.getElementById('initiative-box');
 		initModifier: -1,
 		armour: 12,
 		initiative: 10,
-		nameBox: document.getElementById('combatant3_name'),
-		rollBox: rollBox3
+		comBox: combatantDivs[2],
+		nameBox: nameBoxArr[2],
+		rollBox: rollBoxArr[2]
 	}
 ]
 
 
-//var combatants = [combatant1, combatant2, combatant3];
 
 
 function setInitiativeBox () {
 	var boxH = 0;
 	for (var i=0; i<combatantDivs.length; i++){
 		boxH += combatantDivs[i].offsetHeight;
-		console.log(boxH);
 	}
 	boxH = boxH + (combatantDivs.length * 20) + "px";
 	
@@ -90,6 +81,8 @@ function setUp () {
 	}
 	let iBox = setInitiativeBox();
 	initiativeBox.style.height = iBox;
+
+	sortCombatOrder();
 }
 
 window.onload = setUp;
@@ -97,11 +90,13 @@ window.onload = setUp;
 
 function sortCombatOrder(){
 	combatants.sort((a, b) => b.initiative - a.initiative);
-	console.log(combatants);
-	for ( var i = 0; i< combatants.length; i++){
-		var pos = i * 110 +"px";
-		combatants[i].nameBox.parentElement.style.top = pos;
-		console.log(combatants[i].nameBox.parentElement);
+	let shift = 0;
+	for ( let i = 0; i< combatants.length; i++){
+		if (i > 0) {
+			shift += 10 + combatants[i-1].comBox.offsetHeight;
+		}
+		var pos = shift +"px";
+		combatants[i].comBox.style.top = pos;
 	}
 
 }
@@ -128,7 +123,7 @@ function rollInitiative () {
 }
 
 function quickTest () {
-	console.log(combatantDivs);
+	console.log(rollBoxArr);
 }
 
 
